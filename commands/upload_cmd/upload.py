52e90111e980d65f58bab9991a7e3d3f6ed39f8f
Pablo García <pablogmesa00@gmail.com>
2021-08-01 02:25:14 +0200

    Añadiendo upload

--- commands/upload_cmd/upload.py
+++ commands/upload_cmd/upload.py
@@ -16,19 +16,20 @@ def get_upload_cmd() -> Command:
     upload = Command(
         'upload', description=msg
     )
-    
+
     return upload
 
 upload_logger = logging.getLogger(__name__)
 def upload(args:list=[]) -> None:
+    # Descargamos la base de datos de github y eliminamos el registro anterior de la
+    # mongoapp para reemplazarlo
     download(); ddbb_path = BASE_DIR/'database'; rel_db_app_path = f'database/{DDBB_CLOUD_NAME}'
-    # files = os.listdir(ddbb_path)
-    # if DDBB_CLOUD_NAME in files: 
-    #     outcome = process.run(f"cd database & del /f/q/s '{DDBB_CLOUD_NAME}' & rmdir /q/s '{DDBB_CLOUD_NAME}'", shell=True)
-    #     if outcome == 1:
-    #         upload_logger.info(" Fallo al eliminar carpeta mongoapp para reemplazar")
-    
-    # Metemos la info de mongo en la carpeta
+    files = os.listdir(ddbb_path)
+    if DDBB_CLOUD_NAME in files:
+        outcome = process.run(f'cd database & del /f/q/s "{DDBB_CLOUD_NAME}" & rmdir /q/s "{DDBB_CLOUD_NAME}"', shell=True)
+        if outcome == 1:
+            upload_logger.info(" Fallo al eliminar carpeta mongoapp para reemplazar")
+    # Metemos la info de mongo en la nueva carpeta (la estamos reemplazando)
     process.run(f'cd database & mkdir "{DDBB_CLOUD_NAME}"', shell=True)
     dbs = dbc.list_dbs(only_app_dbs=True)
     for db in dbs:
@@ -39,3 +40,13 @@ def upload(args:list=[]) -> None:
             path = BASE_DIR/f'{rel_db_app_path}/{db}/{collection}.json'
             with open(path, "w") as file:
                 json.dump(docs, file, indent=4, sort_keys=True)
+    # Actualizamos la informacion en github
+    order = f'cd database'
+    order += ' & git add .'
+    order += f' & git commit -m "Actualizando {DDBB_CLOUD_NAME}"'
+    order += ' & git push origin main'
+    process.run(order, shell=True)
+    # Eliminamos la base de datos anterior descargada (carpeta 'database')
+    outcome = process.run(f'del /f/q/s database & rmdir /q/s database', shell=True)
+    if outcome == 1:
+        upload_logger.info(" Fallo al eliminar carpeta 'database' descargada de github")
