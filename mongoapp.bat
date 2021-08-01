4c7873c291d9f892215dd6bad74d984240da1a7d
Pablo García <pablogmesa00@gmail.com>
2021-08-01 21:18:16 +0200

    .bat file Totalmente acabado

--- mongoapp.bat
+++ mongoapp.bat
@@ -5,5 +5,8 @@ if "%1" == "__start__" (
     @REM start powershell -noexit -command "powershell -command 'cd /d C:\Users\pablo\Desktop'; powershell 'commands'"
     start cmd /k "cd /d C:\Users\pablo\Desktop\Pablo\Proyectos Python\Probando Django\mongodb_app\appvenv\Scripts & activate &    cd /d C:\Users\pablo\Desktop\Pablo\Proyectos Python\Probando Django\mongodb_app\ & python manage.py runserver & exit"
 ) else (
-    cmd /k "cd /d C:\Users\pablo\Desktop\Pablo\Proyectos Python\Probando Django\mongodb_app\appvenv\Scripts & activate  & cd C:\Users\pablo\Desktop\Pablo\Proyectos Python\Probando Django\mongodb_app & python main.py %* & deactivate"
+    cd "C:\Users\pablo\Desktop\Pablo\Proyectos Python\Probando Django\mongodb_app\appvenv\Scripts" & activate
+    cd "C:\Users\pablo\Desktop\Pablo\Proyectos Python\Probando Django\mongodb_app" & python main.py %*
+    deactivate
+    cd %__CD__%
 )
